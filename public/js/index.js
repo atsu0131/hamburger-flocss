c7a8ba0d83a9092bc0b419c9cbc6ee6862b61125
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-09-27 00:26:55 +0900

    add

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@ $(function () {
   $(".c-menu__button").on("click", function () {
     if ($(".p-sidemenu").hasClass("off")) {
       $(".p-sidemenu").removeClass("off");
+      $(".main").addClass("main-off");
       $("#overlay").addClass("is-overlay");
       // $("#overlay").fadeIn();
       // $("#c-is-button").animate({ marginLeft: "400px" }, 300).addClass("on");
     } else {
       $(".p-sidemenu").addClass("off");
+      $(".main").removeClass("main-off");
       $("#overlay").removeClass("is-overlay");
       // $("#overlay").fadeOut();
       // $(".p-sidemenu").animate({ right: "0" }, 300);
@@ -15,10 +17,8 @@ $(function () {
   $(".batsu").on("click", function () {
     // $(".p-sidemenu").animate({ right: "-367px" }, 300);
     $(".p-sidemenu").toggleClass("off");
+    $(".main").removeClass("main-off");
     $("#overlay").toggleClass("is-overlay");
     // $("#overlay").fadeOut();
   });
 });
-
-
-
